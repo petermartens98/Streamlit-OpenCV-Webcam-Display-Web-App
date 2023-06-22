ce7151630a8164b73827a998faaf9aeaa481e577
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-06-21 22:22:38 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import cv2
+import numpy as np 
+import tempfile
+
+def main():
+    st.set_page_config(page_title="Facial Detection")
+    st.title("Webcam Display Steamlit App")
+    st.caption("Powered by OpenCV, Streamlit")
+    cap = cv2.VideoCapture(0)
+    frame_placeholder = st.empty()
+    stop_button_pressed = st.button("Stop")
+    while cap.isOpened() and not stop_button_pressed:
+        ret, frame = cap.read()
+        if not ret:
+            st.write("Video Capture Ended")
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_placeholder.image(frame,channels="RGB")
+        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
